4b2e0dd683d0f2258f1ff251d820027641436e05
akadrac <github@drac.id.au>
2022-08-15 19:52:12 +1000

    block public buckets

--- tf/s3.tf
+++ tf/s3.tf
@@ -11,3 +11,12 @@ resource "aws_s3_bucket_acl" "lambda" {
   bucket = aws_s3_bucket.lambda.id
   acl    = "private"
 }
+
+resource "aws_s3_bucket_public_access_block" "lambda" {
+  bucket = aws_s3_bucket.lambda.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
